be4f002e95e074a444df5bfed90a83eca0a60150
Julieta Ramon <julietalaura_89@hotmail.com>
2013-05-08 19:32:25 -0300

    Correcion de codigo

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -6,7 +6,6 @@ package edu.tallerweb.pptls;
 public class Mano {
 	private Forma forma;
 	private final Resultado[][] matrizResultados = new Resultado[5][5];	
-	
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
@@ -28,40 +27,32 @@ public class Mano {
 		return this.matrizResultados[ this.forma.getValor()][otra.forma.getValor()];
 	}
 	
-private void cargarMatrizResultados(){
-		
+private void cargarMatrizResultados() {
 		this.matrizResultados[Forma.TIJERA.getValor()][Forma.TIJERA.getValor()] = Resultado.EMPATA;
 		this.matrizResultados[Forma.TIJERA.getValor()][Forma.PAPEL.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.TIJERA.getValor()][Forma.PIEDRA.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.TIJERA.getValor()][Forma.SPOCK.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.TIJERA.getValor()][Forma.LAGARTO.getValor()] = Resultado.GANA;
-		
 		this.matrizResultados[Forma.PAPEL.getValor()][Forma.TIJERA.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.PAPEL.getValor()][Forma.PAPEL.getValor()] = Resultado.EMPATA;
 		this.matrizResultados[Forma.PAPEL.getValor()][Forma.PIEDRA.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.PAPEL.getValor()][Forma.SPOCK.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.PAPEL.getValor()][Forma.LAGARTO.getValor()] = Resultado.PIERDE;
-		
 		this.matrizResultados[Forma.PIEDRA.getValor()][Forma.TIJERA.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.PIEDRA.getValor()][Forma.PAPEL.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.PIEDRA.getValor()][Forma.PIEDRA.getValor()] = Resultado.EMPATA;
 		this.matrizResultados[Forma.PIEDRA.getValor()][Forma.SPOCK.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.PIEDRA.getValor()][Forma.LAGARTO.getValor()] = Resultado.GANA;
-
-		
 		this.matrizResultados[Forma.SPOCK.getValor()][Forma.TIJERA.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.SPOCK.getValor()][Forma.PAPEL.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.SPOCK.getValor()][Forma.PIEDRA.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.SPOCK.getValor()][Forma.SPOCK.getValor()] = Resultado.EMPATA;
 		this.matrizResultados[Forma.SPOCK.getValor()][Forma.LAGARTO.getValor()] = Resultado.PIERDE;
-
-	
 		this.matrizResultados[Forma.LAGARTO.getValor()][Forma.TIJERA.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.LAGARTO.getValor()][Forma.PAPEL.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.LAGARTO.getValor()][Forma.PIEDRA.getValor()] = Resultado.PIERDE;
 		this.matrizResultados[Forma.LAGARTO.getValor()][Forma.SPOCK.getValor()] = Resultado.GANA;
 		this.matrizResultados[Forma.LAGARTO.getValor()][Forma.LAGARTO.getValor()] = Resultado.EMPATA;
-
 	}
 
 }
